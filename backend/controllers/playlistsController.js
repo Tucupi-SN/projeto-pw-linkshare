89e3751dcb6a061a3d3c17dcdc32a699d881a062
ivesfg1 <wigzfurtado@gmail.com>
2023-02-07 10:25:11 -0300

    indenting

--- backend/controllers/playlistsController.js
+++ backend/controllers/playlistsController.js
@@ -2,102 +2,102 @@ const { Playlist } = require("../database/models.js");
 const { playlistMapper } = require("../mappers/playlistMapper.js");
 
 const getPlaylistsFeed = async (req, res) => {
-  const allPlaylists = await Playlist.findAll({
-    include: { all: true, nested: true },
-  });
+	const allPlaylists = await Playlist.findAll({
+		include: { all: true, nested: true },
+	});
 
-  const publicPlaylists = allPlaylists.filter(
-    (playlist) => !playlist.isPrivate
-  );
+	const publicPlaylists = allPlaylists.filter(
+		(playlist) => !playlist.isPrivate
+	);
 
-  const mappedPlaylists = publicPlaylists.map((playlist) =>
-    playlistMapper(playlist)
-  );
+	const mappedPlaylists = publicPlaylists.map((playlist) =>
+		playlistMapper(playlist)
+	);
 
-  return res.json(mappedPlaylists);
+	return res.json(mappedPlaylists);
 };
 
 const getPlaylistById = async (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  const playlist = await Playlist.findByPk(id, {
-    include: { all: true, nested: true },
-  });
+	const playlist = await Playlist.findByPk(id, {
+		include: { all: true, nested: true },
+	});
 
-  if (playlist === null) {
-    return res.status(404).json({ message: "No record with the given id" });
-  }
+	if (playlist === null) {
+		return res.status(404).json({ message: "No record with the given id" });
+	}
 
-  const mappedPlaylist = playlistMapper(playlist);
+	const mappedPlaylist = playlistMapper(playlist);
 
-  return res.json(mappedPlaylist);
+	return res.json(mappedPlaylist);
 };
 
 const createPlaylist = async (req, res) => {
-  const { body } = req;
+	const { body } = req;
 
-  if (
-    body.name === undefined ||
-    body.name === "" ||
-    body.image === undefined ||
-    body.image === "" ||
-    body.profileId === undefined
-  ) {
-    return res.status(400).json({
-      message: "Mandatory fields must be informed and cannot be empty",
-    });
-  }
+	if (
+		body.name === undefined ||
+		body.name === "" ||
+		body.image === undefined ||
+		body.image === "" ||
+		body.profileId === undefined
+	) {
+		return res.status(400).json({
+			message: "Mandatory fields must be informed and cannot be empty",
+		});
+	}
 
-  const newPlaylist = await Playlist.create(body);
+	const newPlaylist = await Playlist.create(body);
 
-  return res.status(201).json(newPlaylist);
+	return res.status(201).json(newPlaylist);
 };
 
 const updatePlaylist = async (req, res) => {
-  const { id } = req.params;
-  const { body } = req;
+	const { id } = req.params;
+	const { body } = req;
 
-  if (body.name === "" || body.image === "") {
-    return res
-      .status(400)
-      .json({ message: "Mandatory fields cannot be empty" });
-  }
+	if (body.name === "" || body.image === "") {
+		return res
+			.status(400)
+			.json({ message: "Mandatory fields cannot be empty" });
+	}
 
-  const playlist = await Playlist.findByPk(id, {
-    include: { all: true, nested: true },
-  });
+	const playlist = await Playlist.findByPk(id, {
+		include: { all: true, nested: true },
+	});
 
-  if (playlist === null) {
-    return res.status(404).json({ message: "No record with the given id" });
-  }
+	if (playlist === null) {
+		return res.status(404).json({ message: "No record with the given id" });
+	}
 
-  const updatedPlaylist = await playlist.update(body);
+	const updatedPlaylist = await playlist.update(body);
 
-  const mappedPlaylist = playlistMapper(updatedPlaylist);
+	const mappedPlaylist = playlistMapper(updatedPlaylist);
 
-  return res.json(mappedPlaylist);
+	return res.json(mappedPlaylist);
 };
 
 const deletePlaylist = async (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  const playlist = await Playlist.findByPk(id, {
-    include: { all: true, nested: true },
-  });
+	const playlist = await Playlist.findByPk(id, {
+		include: { all: true, nested: true },
+	});
 
-  if (playlist === null) {
-    return res.status(404).json({ message: "No record with the given id" });
-  }
+	if (playlist === null) {
+		return res.status(404).json({ message: "No record with the given id" });
+	}
 
-  await playlist.destroy();
+	await playlist.destroy();
 
-  return res.json({ message: "Record successfuly deleted" });
+	return res.json({ message: "Record successfuly deleted" });
 };
 
 module.exports = {
-  getPlaylistsFeed,
-  getPlaylistById,
-  createPlaylist,
-  updatePlaylist,
-  deletePlaylist,
+	getPlaylistsFeed,
+	getPlaylistById,
+	createPlaylist,
+	updatePlaylist,
+	deletePlaylist,
 };
