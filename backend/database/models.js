3a12b32c3158eb5155f11d9ef9abf45b47e48eb1
Alysson Aldrin <alysson_cdf@hotmail.com>
2023-02-07 21:52:04 -0300

    Adicionando autenticacao

--- backend/database/models.js
+++ backend/database/models.js
@@ -1,129 +1,108 @@
 const { DataTypes } = require("sequelize");
 const database = require("./config.js");
-const crypto = require("crypto");
 
 const Playlist = database.define(
-	"Playlist",
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			autoIncrement: true,
-			primaryKey: true,
-		},
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		image: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		createdAt: {
-			type: DataTypes.DATEONLY,
-			allowNull: false,
-			defaultValue: DataTypes.NOW,
-		},
-		isPrivate: {
-			type: DataTypes.BOOLEAN,
-			allowNull: false,
-			defaultValue: true,
-		},
-	},
-	{ updatedAt: false }
+  "Playlist",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    isPrivate: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  { updatedAt: false }
 );
 
 const Profile = database.define(
-	"Profile",
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			autoIncrement: true,
-			primaryKey: true,
-		},
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: true,
-		},
-		profilePicture: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-	},
-	{ updatedAt: false }
+  "Profile",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { updatedAt: false }
 );
 
-Profile.prototype.setPassword = function (password) {
-	this.salt = crypto.randomBytes(16);
-	this.password = crypto
-		.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
-		.toString(`hex`);
-};
-
-// Profile.prototype.validatePassword = function (password) {
-// 	const hash = crypto
-// 		.pbkdf2Sync(
-// 			password,
-// 			crypto.randomBytes(16).toString("hex"),
-// 			1000,
-// 			64,
-// 			`sha512`
-// 		)
-// 		.toString(`hex`);
-// 	return this.password === hash;
-// };
-
 const Music = database.define(
-	"Music",
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			autoIncrement: true,
-			primaryKey: true,
-		},
-		title: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		artist: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		duration: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-		},
-		musicStyle: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		url: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-	},
-	{ updatedAt: false }
+  "Music",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    artist: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    musicStyle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { updatedAt: false }
 );
 
 Playlist.hasMany(Music, {
-	foreignKey: { name: "playlistId", allowNull: false },
+  foreignKey: { name: "playlistId", allowNull: false },
 });
 Music.belongsTo(Playlist, {
-	foreignKey: { name: "playlistId", allowNull: false },
+  foreignKey: { name: "playlistId", allowNull: false },
 });
 Profile.hasMany(Playlist, {
-	foreignKey: { name: "profileId", allowNull: false },
+  foreignKey: { name: "profileId", allowNull: false },
 });
 Playlist.belongsTo(Profile, {
-	foreignKey: { name: "profileId", allowNull: false },
+  foreignKey: { name: "profileId", allowNull: false },
 });
 
 module.exports = { Playlist, Profile, Music };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,96 +4,64 @@ const router = express.Router();
 const playlistsController = require("../controllers/playlistsController.js");
 const musicsController = require("../controllers/musicsController.js");
 const profilesController = require("../controllers/profilesController.js");
+const { isAuthenticated } = require("../middlewares");
 
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const crypto = require("crypto");
-const db = require("../database/config");
-
-const { Profile } = require("../database/models.js");
-
-passport.use(
-	new LocalStrategy(function verify(email, password, cb) {
-		db.get(
-			"SELECT * FROM Profile WHERE email = ?",
-			[email],
-			function (err, user) {
-				if (err) {
-					return cb(err);
-				}
-				if (!user) {
-					return cb(null, false, {
-						message: "Incorrect email or password.",
-					});
-				}
-
-				crypto.pbkdf2(
-					password,
-					row.salt,
-					1000,
-					64,
-					"sha512",
-					function (err, hashedPassword) {
-						if (err) {
-							return cb(err);
-						}
-						if (
-							!crypto.timingSafeEqual(
-								user.password,
-								hashedPassword
-							)
-						) {
-							return cb(null, false, {
-								message: "Incorrect email or password.",
-							});
-						}
-						return cb(null, user);
-					}
-				);
-			}
-		);
-	})
-);
-
-router.get("/login/failed", (req, res) => {
-	return res.json("Login falhou");
-});
-
-router.post(
-	"/login",
-	passport.authenticate("local", {
-		successRedirect: "/playlists/public",
-		failureRedirect: "/login/failed",
-	})
-);
-
-module.exports = router;
+router.post("/login", profilesController.login);
+router.get("/logout", isAuthenticated, profilesController.logout);
 
 // endpoints de Playlist
 
-router.get("/playlists/public", playlistsController.getPlaylistsFeed);
-router.post("/playlists", playlistsController.createPlaylist);
-router.get("/playlists/:id", playlistsController.getPlaylistById);
-router.get("/playlists/:id/musics", musicsController.getMusicsByPlaylist);
-router.patch("/playlists/:id", playlistsController.updatePlaylist);
-router.delete("/playlists/:id", playlistsController.deletePlaylist);
+router.get(
+  "/playlists/public",
+  isAuthenticated,
+  playlistsController.getPlaylistsFeed
+);
+router.post("/playlists", isAuthenticated, playlistsController.createPlaylist);
+router.get(
+  "/playlists/:id",
+  isAuthenticated,
+  playlistsController.getPlaylistById
+);
+router.get(
+  "/playlists/:id/musics",
+  isAuthenticated,
+  musicsController.getMusicsByPlaylist
+);
+router.patch(
+  "/playlists/:id",
+  isAuthenticated,
+  playlistsController.updatePlaylist
+);
+router.delete(
+  "/playlists/:id",
+  isAuthenticated,
+  playlistsController.deletePlaylist
+);
 
 // endpoints de Profile
 
-router.get("/profiles/:id", profilesController.getProfileById);
+router.get("/profiles/:id", isAuthenticated, profilesController.getProfileById);
 router.get(
-	"/profiles/:id/playlists",
-	profilesController.getPlaylistsByProfileId
+  "/profiles/:id/playlists",
+  profilesController.getPlaylistsByProfileId
 );
 router.post("/profiles", profilesController.createProfile);
-router.patch("/profiles/:id", profilesController.updateProfile);
-router.delete("/profiles/:id", profilesController.deleteProfile);
+router.patch(
+  "/profiles/:id",
+  isAuthenticated,
+  profilesController.updateProfile
+);
+router.delete(
+  "/profiles/:id",
+  isAuthenticated,
+  profilesController.deleteProfile
+);
 
 // endpoints de Music
 
-router.get("/musics/:id", musicsController.getMusicById);
-router.post("/musics", musicsController.createMusic);
-router.patch("/musics/:id", musicsController.updateMusic);
-router.delete("/musics/:id", musicsController.deleteMusic);
+router.get("/musics/:id", isAuthenticated, musicsController.getMusicById);
+router.post("/musics", isAuthenticated, musicsController.createMusic);
+router.patch("/musics/:id", isAuthenticated, musicsController.updateMusic);
+router.delete("/musics/:id", isAuthenticated, musicsController.deleteMusic);
 
 module.exports = router;
