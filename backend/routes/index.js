b4c0f7fee7449da5916a84fab3708dbdcf6a28b8
ivesfg1 <wigzfurtado@gmail.com>
2023-02-08 02:37:52 -0300

    create e update de music funcional

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -19,13 +19,19 @@ router.get("/dashboard", async (req, res) => {
 			.then((data) => {
 				element.numberOfMusics = data.length;
 
-				element.musicNames = "";
+				element.musicNames = [];
 
 				for (let i = 0; i < data.length; i++) {
 					if (i !== data.length - 1) {
-						element.musicNames += `${i + 1} - ${data[i].title}, `;
+						element.musicNames.push({
+							id: data[i].id,
+							name: `${i + 1} - ${data[i].title}, `,
+						});
 					} else {
-						element.musicNames += `${i + 1} - ${data[i].title}`;
+						element.musicNames.push({
+							id: data[i].id,
+							name: `${i + 1} - ${data[i].title}`,
+						});
 					}
 				}
 			});
@@ -48,13 +54,19 @@ router.get("/profile-dashboard", async (req, res) => {
 			.then((data) => {
 				element.numberOfMusics = data.length;
 
-				element.musicNames = "";
+				element.musicNames = [];
 
 				for (let i = 0; i < data.length; i++) {
 					if (i !== data.length - 1) {
-						element.musicNames += `${i + 1} - ${data[i].title}, `;
+						element.musicNames.push({
+							id: data[i].id,
+							name: `${i + 1} - ${data[i].title}, `,
+						});
 					} else {
-						element.musicNames += `${i + 1} - ${data[i].title}`;
+						element.musicNames.push({
+							id: data[i].id,
+							name: `${i + 1} - ${data[i].title}`,
+						});
 					}
 				}
 			});
@@ -110,8 +122,6 @@ router.post("/musics", async (req, res) => {
 
 	await Music.create(newMusic);
 
-	console.log(newMusic);
-
 	context = {
 		playlistChoices: await fetch(
 			"http://localhost:3000/api/profiles/1/playlists"
@@ -119,7 +129,79 @@ router.post("/musics", async (req, res) => {
 	};
 
 	res.redirect("/dashboard");
-	// res.render("add_music.html", context);
+});
+
+router.get("/musics/:id/edit", async (req, res) => {
+	context = {
+		playlistChoices: await fetch(
+			"http://localhost:3000/api/profiles/1/playlists"
+		).then((response) => response.json()),
+		currentMusic: await fetch(
+			`http://localhost:3000/api/musics/${req.params.id}`
+		).then((response) => response.json()),
+	};
+	res.render("editar_musica.html", context);
+});
+
+router.post("/musics/:id/edit", async (req, res) => {
+	// context = {
+	// 	playlistChoices: await fetch(
+	// 		"http://localhost:3000/api/profiles/1/playlists"
+	// 	).then((response) => response.json()),
+	// 	,
+	// };
+
+	let musicStyles = {
+		1: "Rock",
+		2: "Blues",
+		3: "Jazz",
+		4: "Pop",
+		5: "Country",
+		6: "Reggae",
+		7: "Axé",
+		8: "Bossa Nova",
+		9: "Clássico",
+		10: "Folk",
+		11: "Música Eletrônica",
+		12: "Gospel/Religioso",
+		13: "Forró",
+		14: "Hip Hop",
+		15: "Instrumental",
+		16: "MPB",
+		17: "Rap",
+		18: "Progressivo",
+		19: "R&B",
+		20: "Samba",
+		21: "Trap",
+		22: "Soul",
+		23: "Pagode",
+		24: "Outro",
+	};
+
+	let currentMusic = await fetch(
+		`http://localhost:3000/api/musics/${req.params.id}`
+	).then((response) => response.json());
+
+	let musicPayload = {
+		title: req.body.title,
+		artist: req.body.artist,
+		duration: req.body.duration,
+		musicStyle: req.body["musics-styles"]
+			? musicStyles[parseInt(req.body["musics-styles"])]
+			: currentMusic.musicStyle,
+		playlistId: req.body.playlistChoice
+			? parseInt(req.body.playlistChoice)
+			: currentMusic.playlist.id,
+		url: req.body.url,
+	};
+
+	console.log(musicPayload);
+
+	let musicToUpdate = await Music.findByPk(req.params.id);
+	musicToUpdate.update(musicPayload);
+	await musicToUpdate.save();
+
+	res.redirect("/profile-dashboard");
 });
 
 module.exports = router;
