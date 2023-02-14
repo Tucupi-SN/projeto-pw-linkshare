1b3dfb6ad5a2d9b8c5df275034825ec27a6b703a
Alysson Aldrin <alysson_cdf@hotmail.com>
2023-02-13 23:56:23 -0300

    Correçao teste

--- src/__tests__/controllers/musicsController.test.js
+++ src/__tests__/controllers/musicsController.test.js
@@ -4,248 +4,248 @@ import Models from "../../database/models.js";
 jest.mock("../../database/models.js");
 
 afterEach(() => {
-	jest.resetAllMocks();
+  jest.resetAllMocks();
 });
 
 const musicsMock = [
-	{
-		id: 1,
-		title: "Hey Jude",
-		artist: "The Beatles",
-		duration: 10,
-		musicStyle: "Rock",
-		url: "musicurl.com",
-		createdAt: new Date(2023, 1, 1),
-		Playlist: {
-			id: 1,
-			name: "Melhores",
-			image: "image.png",
-			createdAt: new Date(2023, 1, 1),
-			isPrivate: true,
-			Profile: {
-				id: 1,
-				name: "Diogo",
-				email: "diogo@gmail.com",
-				profilePicture: "image.png",
-				createdAt: new Date(2023, 1, 1),
-			},
-		},
-	},
+  {
+    id: 1,
+    title: "Hey Jude",
+    artist: "The Beatles",
+    duration: 10,
+    musicStyle: "Rock",
+    url: "musicurl.com",
+    createdAt: new Date(2023, 1, 1),
+    Playlist: {
+      id: 1,
+      name: "Melhores",
+      image: "image.png",
+      createdAt: new Date(2023, 1, 1),
+      isPrivate: true,
+      Profile: {
+        id: 1,
+        name: "Diogo",
+        email: "diogo@gmail.com",
+        profilePicture: "image.png",
+        createdAt: new Date(2023, 1, 1),
+      },
+    },
+  },
 ];
 
 describe("createMusic", () => {
-	test("Deve cadastrar nova musica com sucesso", async () => {
-		// Arrange
-		const musicMock = { ...musicsMock[0] };
-		const musicCreatedMock = {
-			id: musicMock.id,
-			title: musicMock.title,
-			artist: musicMock.artist,
-			duration: musicMock.duration,
-			musicStyle: musicMock.musicStyle,
-			url: musicMock.url,
-			createdAt: musicMock.createdAt,
-			playlistId: musicMock.Playlist.id,
-		};
+  test("Deve cadastrar nova musica com sucesso", async () => {
+    // Arrange
+    const musicMock = { ...musicsMock[0] };
+    const musicCreatedMock = {
+      id: musicMock.id,
+      title: musicMock.title,
+      artist: musicMock.artist,
+      duration: musicMock.duration,
+      musicStyle: musicMock.musicStyle,
+      url: musicMock.url,
+      createdAt: musicMock.createdAt,
+      playlistId: musicMock.Playlist.id,
+    };
 
-		const expectedMusic = {
-			id: 1,
-			title: "Hey Jude",
-			artist: "The Beatles",
-			duration: 10,
-			musicStyle: "Rock",
-			url: "musicurl.com",
-			createdAt: new Date(2023, 1, 1),
-			playlistId: 1,
-		};
+    const expectedMusic = {
+      id: 1,
+      title: "Hey Jude",
+      artist: "The Beatles",
+      duration: 10,
+      musicStyle: "Rock",
+      url: "musicurl.com",
+      createdAt: new Date(2023, 1, 1),
+      playlistId: 1,
+    };
 
-		const mockBody = { ...musicsMock[0] };
-		const mockRequest = {
-			body: {
-				title: mockBody.title,
-				artist: mockBody.artist,
-				duration: mockBody.duration,
-				musicStyle: mockBody.musicStyle,
-				url: mockBody.url,
-				createdAt: mockBody.createdAt,
-				playlistId: mockBody.Playlist.id,
-			},
-		};
-		const mockResponse = {
-			json: jest.fn().mockReturnThis(),
-			status: jest.fn().mockReturnThis(),
-		};
+    const mockBody = { ...musicsMock[0] };
+    const mockRequest = {
+      body: {
+        title: mockBody.title,
+        artist: mockBody.artist,
+        duration: mockBody.duration,
+        musicStyle: mockBody.musicStyle,
+        url: mockBody.url,
+        createdAt: mockBody.createdAt,
+        playlistId: mockBody.Playlist.id,
+      },
+    };
+    const mockResponse = {
+      json: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+    };
 
-		jest.spyOn(Models.Music, "create").mockResolvedValue(musicCreatedMock);
+    jest.spyOn(Models.Music, "create").mockResolvedValue(musicCreatedMock);
 
-		// Act
-		const actualResponse = await musicsController.createMusic(
-			mockRequest,
-			mockResponse
-		);
+    // Act
+    const actualResponse = await musicsController.createMusic(
+      mockRequest,
+      mockResponse
+    );
 
-		// Assert
-		expect(Models.Music.create).toHaveBeenCalledTimes(1);
-		expect(actualResponse.status).toBeCalledWith(201);
-		expect(actualResponse.json).toBeCalledWith(expectedMusic);
-	});
+    // Assert
+    expect(Models.Music.create).toHaveBeenCalledTimes(1);
+    expect(actualResponse.status).toBeCalledWith(201);
+    expect(actualResponse.json).toBeCalledWith(expectedMusic);
+  });
 
-	test("Não deve cadastrar nova música com informações obrigatórias faltando", async () => {
-		// Arrange
-		const mockBody = { ...musicsMock[0] };
-		const mockRequest = {
-			body: {
-				title: mockBody.title,
-				playlistId: mockBody.Playlist.id,
-			},
-		};
-		const mockResponse = {
-			json: jest.fn().mockReturnThis(),
-			status: jest.fn().mockReturnThis(),
-		};
+  test("Não deve cadastrar nova música com informações obrigatórias faltando", async () => {
+    // Arrange
+    const mockBody = { ...musicsMock[0] };
+    const mockRequest = {
+      body: {
+        title: mockBody.title,
+        playlistId: mockBody.Playlist.id,
+      },
+    };
+    const mockResponse = {
+      json: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+    };
 
-		// Act
-		const actualResponse = await musicsController.createMusic(
-			mockRequest,
-			mockResponse
-		);
+    // Act
+    const actualResponse = await musicsController.createMusic(
+      mockRequest,
+      mockResponse
+    );
 
-		// Assert
-		expect(actualResponse.status).toBeCalledWith(400);
-		expect(actualResponse.json).toBeCalledWith({
-			message: "Mandatory fields must be informed and cannot be empty",
-		});
-	});
+    // Assert
+    expect(actualResponse.status).toBeCalledWith(400);
+    expect(actualResponse.json).toBeCalledWith({
+      message: "Mandatory fields must be informed and cannot be empty",
+    });
+  });
 });
 
 describe("getMusicsById", () => {
-	test("Deve retornar a musica quando id informado existir no banco", async () => {
-		// Arrange
-		const musicFoundMock = { ...musicsMock[0] };
+  test("Deve retornar a musica quando id informado existir no banco", async () => {
+    // Arrange
+    const musicFoundMock = { ...musicsMock[0] };
 
-		const expectedMusic = {
-			id: 1,
-			title: "Hey Jude",
-			artist: "The Beatles",
-			duration: 10,
-			musicStyle: "Rock",
-			url: "musicurl.com",
-			createdAt: new Date(2023, 1, 1),
-			playlist: {
-				id: 1,
-				name: "Melhores",
-				image: "image.png",
-				createdAt: new Date(2023, 1, 1),
-				isPrivate: true,
-				profile: {
-					id: 1,
-					name: "Diogo",
-					email: "diogo@gmail.com",
-					profilePicture: "image.png",
-					createdAt: new Date(2023, 1, 1),
-				},
-			},
-		};
+    const expectedMusic = {
+      id: 1,
+      title: "Hey Jude",
+      artist: "The Beatles",
+      duration: 10,
+      musicStyle: "Rock",
+      url: "musicurl.com",
+      createdAt: new Date(2023, 1, 1),
+      playlist: {
+        id: 1,
+        name: "Melhores",
+        image: "image.png",
+        createdAt: new Date(2023, 1, 1),
+        isPrivate: true,
+        profile: {
+          id: 1,
+          name: "Diogo",
+          email: "diogo@gmail.com",
+          profilePicture: "image.png",
+          createdAt: new Date(2023, 1, 1),
+        },
+      },
+    };
 
-		const mockRequest = { params: { id: 1 } };
-		const mockResponse = {
-			json: jest.fn().mockReturnThis(),
-		};
+    const mockRequest = { params: { id: 1 } };
+    const mockResponse = {
+      json: jest.fn().mockReturnThis(),
+    };
 
-		jest.spyOn(Models.Music, "findByPk").mockResolvedValue(musicFoundMock);
+    jest.spyOn(Models.Music, "findByPk").mockResolvedValue(musicFoundMock);
 
-		// Act
-		const actualResponse = await musicsController.getMusicById(
-			mockRequest,
-			mockResponse
-		);
+    // Act
+    const actualResponse = await musicsController.getMusicById(
+      mockRequest,
+      mockResponse
+    );
 
-		// Assert
-		expect(Models.Music.findByPk).toHaveBeenCalledTimes(1);
-		expect(actualResponse.json).toBeCalledWith(expectedMusic);
-	});
+    // Assert
+    expect(Models.Music.findByPk).toHaveBeenCalledTimes(1);
+    expect(actualResponse.json).toBeCalledWith(expectedMusic);
+  });
 
-	test("Deve retornar status 404 e mensagem de error quando não existir id no banco", async () => {
-		// Arrange
-		const mockRequest = { params: { id: 3 } };
-		const mockResponse = {
-			json: jest.fn().mockReturnThis(),
-			status: jest.fn().mockReturnThis(),
-		};
+  test("Deve retornar status 404 e mensagem de error quando não existir id no banco", async () => {
+    // Arrange
+    const mockRequest = { params: { id: 3 } };
+    const mockResponse = {
+      json: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+    };
 
-		jest.spyOn(Models.Music, "findByPk").mockResolvedValue(null);
+    jest.spyOn(Models.Music, "findByPk").mockResolvedValue(null);
 
-		// Act
-		const actualResponse = await musicsController.getMusicById(
-			mockRequest,
-			mockResponse
-		);
+    // Act
+    const actualResponse = await musicsController.getMusicById(
+      mockRequest,
+      mockResponse
+    );
 
-		// Assert
-		expect(Models.Music.findByPk).toHaveBeenCalledTimes(1);
-		expect(actualResponse.status).toBeCalledWith(404);
-		expect(actualResponse.json).toBeCalledWith({
-			message: "No record with the given id",
-		});
-	});
+    // Assert
+    expect(Models.Music.findByPk).toHaveBeenCalledTimes(1);
+    expect(actualResponse.status).toBeCalledWith(404);
+    expect(actualResponse.json).toBeCalledWith({
+      message: "No record with the given id",
+    });
+  });
 });
 
 describe("updateMusic", () => {
-	test("Deve alterar dados de uma musica com sucesso", async () => {
-		// Arrange
-		const musicUpdatedMock = {
-			...musicsMock[0],
-			name: "Hey Jude Editada",
-		};
-		const musicFoundMock = {
-			...musicsMock[0],
-			update: jest.fn().mockResolvedValue(musicUpdatedMock),
-		};
+  test("Deve alterar dados de uma musica com sucesso", async () => {
+    // Arrange
+    const musicUpdatedMock = {
+      ...musicsMock[0],
+      title: "Hey Jude Editada",
+    };
+    const musicFoundMock = {
+      ...musicsMock[0],
+      update: jest.fn().mockResolvedValue(musicUpdatedMock),
+    };
 
-		const expectedMusic = {
-			id: 1,
-			title: "Hey Jude Editada",
-			artist: "The Beatles",
-			duration: 10,
-			musicStyle: "Rock",
-			url: "musicurl.com",
-			createdAt: new Date(2023, 1, 1),
-			playlist: {
-				id: 1,
-				name: "Melhores",
-				image: "image.png",
-				createdAt: new Date(2023, 1, 1),
-				isPrivate: true,
-				profile: {
-					id: 1,
-					name: "Diogo",
-					email: "diogo@gmail.com",
-					profilePicture: "image.png",
-					createdAt: new Date(2023, 1, 1),
-				},
-			},
-		};
+    const expectedMusic = {
+      id: 1,
+      title: "Hey Jude Editada",
+      artist: "The Beatles",
+      duration: 10,
+      musicStyle: "Rock",
+      url: "musicurl.com",
+      createdAt: new Date(2023, 1, 1),
+      playlist: {
+        id: 1,
+        name: "Melhores",
+        image: "image.png",
+        createdAt: new Date(2023, 1, 1),
+        isPrivate: true,
+        profile: {
+          id: 1,
+          name: "Diogo",
+          email: "diogo@gmail.com",
+          profilePicture: "image.png",
+          createdAt: new Date(2023, 1, 1),
+        },
+      },
+    };
 
-		const mockRequest = {
-			params: { id: 1 },
-			body: {
-				name: "Hey Jude Editada",
-			},
-		};
-		const mockResponse = {
-			json: jest.fn().mockReturnThis(),
-		};
+    const mockRequest = {
+      params: { id: 1 },
+      body: {
+        title: "Hey Jude Editada",
+      },
+    };
+    const mockResponse = {
+      json: jest.fn().mockReturnThis(),
+    };
 
-		jest.spyOn(Models.Music, "findByPk").mockResolvedValue(musicFoundMock);
+    jest.spyOn(Models.Music, "findByPk").mockResolvedValue(musicFoundMock);
 
-		// Act
-		const actualResponse = await musicsController.updateMusic(
-			mockRequest,
-			mockResponse
-		);
+    // Act
+    const actualResponse = await musicsController.updateMusic(
+      mockRequest,
+      mockResponse
+    );
 
-		// Assert
-		expect(Models.Music.findByPk).toHaveBeenCalledTimes(1);
-		expect(actualResponse.json).toBeCalledWith(expectedMusic);
-	});
+    // Assert
+    expect(Models.Music.findByPk).toHaveBeenCalledTimes(1);
+    expect(actualResponse.json).toBeCalledWith(expectedMusic);
+  });
 });
--- src/controllers/musicsController.js
+++ src/controllers/musicsController.js
@@ -2,114 +2,114 @@ const { Music } = require("../database/models.js");
 const { musicMapper } = require("../mappers/musicMapper.js");
 
 const getMusicsByPlaylist = async (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const allMusics = await Music.findAll({
-		include: { all: true, nested: true },
-	});
+  const allMusics = await Music.findAll({
+    include: { all: true, nested: true },
+  });
 
-	const musicsByPlaylist = allMusics.filter(
-		(music) => music.playlistId === parseInt(id)
-	);
+  const musicsByPlaylist = allMusics.filter(
+    (music) => music.playlistId === parseInt(id)
+  );
 
-	const mappedMusics = musicsByPlaylist.map((music) => musicMapper(music));
+  const mappedMusics = musicsByPlaylist.map((music) => musicMapper(music));
 
-	return res.json(mappedMusics);
+  return res.json(mappedMusics);
 };
 
 const getMusicById = async (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const music = await Music.findByPk(id, {
-		include: { all: true, nested: true },
-	});
+  const music = await Music.findByPk(id, {
+    include: { all: true, nested: true },
+  });
 
-	if (music === null) {
-		return res.status(404).json({ message: "No record with the given id" });
-	}
+  if (music === null) {
+    return res.status(404).json({ message: "No record with the given id" });
+  }
 
-	const mappedMusic = musicMapper(music);
+  const mappedMusic = musicMapper(music);
 
-	return res.json(mappedMusic);
+  return res.json(mappedMusic);
 };
 
 const createMusic = async (req, res) => {
-	const { body } = req;
+  const { body } = req;
 
-	if (
-		body.title === undefined ||
-		body.title === "" ||
-		body.artist === undefined ||
-		body.artist === "" ||
-		body.duration === undefined ||
-		body.duration === "" ||
-		body.musicStyle === undefined ||
-		body.musicStyle === "" ||
-		body.url === undefined ||
-		body.url === "" ||
-		body.playlistId === undefined
-	) {
-		return res.status(400).json({
-			message: "Mandatory fields must be informed and cannot be empty",
-		});
-	}
+  if (
+    body.title === undefined ||
+    body.title === "" ||
+    body.artist === undefined ||
+    body.artist === "" ||
+    body.duration === undefined ||
+    body.duration === "" ||
+    body.musicStyle === undefined ||
+    body.musicStyle === "" ||
+    body.url === undefined ||
+    body.url === "" ||
+    body.playlistId === undefined
+  ) {
+    return res.status(400).json({
+      message: "Mandatory fields must be informed and cannot be empty",
+    });
+  }
 
-	const newMusic = await Music.create(body);
+  const newMusic = await Music.create(body);
 
-	return res.status(201).json(newMusic);
+  return res.status(201).json(newMusic);
 };
 
 const updateMusic = async (req, res) => {
-	const { id } = req.params;
-	const { body } = req;
+  const { id } = req.params;
+  const { body } = req;
 
-	if (
-		body.title === "" ||
-		body.artist === "" ||
-		body.duration === "" ||
-		body.musicStyle === "" ||
-		body.url === ""
-	) {
-		return res
-			.status(400)
-			.json({ message: "Mandatory fields cannot be empty" });
-	}
+  if (
+    body.title === "" ||
+    body.artist === "" ||
+    body.duration === "" ||
+    body.musicStyle === "" ||
+    body.url === ""
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Mandatory fields cannot be empty" });
+  }
 
-	const music = await Music.findByPk(id, {
-		include: { all: true, nested: true },
-	});
+  const music = await Music.findByPk(id, {
+    include: { all: true, nested: true },
+  });
 
-	if (music === null) {
-		return res.status(404).json({ message: "No record with the given id" });
-	}
+  if (music === null) {
+    return res.status(404).json({ message: "No record with the given id" });
+  }
 
-	await music.update(body);
+  const updatedMusic = await music.update(body);
 
-	const mappedMusic = musicMapper(music);
+  const mappedMusic = musicMapper(updatedMusic);
 
-	return res.json(mappedMusic);
+  return res.json(mappedMusic);
 };
 
 const deleteMusic = async (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const music = await Music.findByPk(id, {
-		include: { all: true, nested: true },
-	});
+  const music = await Music.findByPk(id, {
+    include: { all: true, nested: true },
+  });
 
-	if (music === null) {
-		return res.status(404).json({ message: "No record with the given id" });
-	}
+  if (music === null) {
+    return res.status(404).json({ message: "No record with the given id" });
+  }
 
-	await music.destroy();
+  await music.destroy();
 
-	return res.json({ message: "Record successfuly deleted" });
+  return res.json({ message: "Record successfuly deleted" });
 };
 
 module.exports = {
-	getMusicsByPlaylist,
-	getMusicById,
-	createMusic,
-	updateMusic,
-	deleteMusic,
+  getMusicsByPlaylist,
+  getMusicById,
+  createMusic,
+  updateMusic,
+  deleteMusic,
 };
